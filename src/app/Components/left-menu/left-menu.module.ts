190c226263825b883b8882cda4c7e88ee3d0f30a
davodbeheshti <davood.beheshti2018@gmail.com>
2021-10-19 23:37:25 +0330

    changes Files in project

--- src/app/Components/left-menu/left-menu.module.ts
+++ src/app/Components/left-menu/left-menu.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeftMenuComponent } from './left-menu.component';
 import { ListIemsToolsComponent } from './list-iems-tools/list-iems-tools.component';
+import { CreateGroupComponent } from './list-iems-tools/create-group/create-group.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
     LeftMenuComponent,
-    ListIemsToolsComponent
+    ListIemsToolsComponent,
+    CreateGroupComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports : [
     LeftMenuComponent
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
+let importComponentMaterial = [
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  
+]
 
+let exportedComponent = [
+  ...importComponentMaterial,  
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ...importComponentMaterial,
   ],
-  exports: []
+  exports: [...exportedComponent,]
 })
 export class SharedModule { }

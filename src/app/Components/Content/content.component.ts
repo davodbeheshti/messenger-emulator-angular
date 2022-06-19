bb8e6c0bc8d577c885e300f11a112668679d42ba
davodbeheshti <davood.beheshti2018@gmail.copm>
2022-06-19 11:16:29 -0700

    fixes changes content

--- src/app/Components/Content/content.component.ts
+++ src/app/Components/Content/content.component.ts
@@ -1,3 +1,4 @@
+import { CurentUser, users, messages } from '../../shared/IModelProject';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -7,44 +8,46 @@ import { ForwardMessageComponent } from './forward-message/forward-message.compo
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  styleUrls: ['./content.component.scss'],
 })
 export class ContentComponent implements OnInit {
-
-  currentUser;
+  currentUser: CurentUser;
   userId: string;
-  users;
+  users: users[] = [];
   sendMessge: string = '';
-  messages = [];
+  messages: messages[] = [];
   isEdit: boolean = false;
-  indexItemActiveInEdit: number;
   showIconOperationById: string = '';
   pinMessageBox: boolean = false;
   messageBoxPin: string = '';
   viewEditMessage: boolean = false;
   messageBoxEdit: string = '';
   timeSystem: string;
-  listUserFromLS;
-  
-  constructor(private route: ActivatedRoute, private router: Router,
-    private cdr: ChangeDetectorRef, private service: ProjectService, public dialog: MatDialog) {
-    this.listUserFromLS = JSON.parse(window.localStorage.getItem('user'));
-    console.log(this.listUserFromLS)
-    this.userId = this.route.snapshot.paramMap.get('id');
-    this.currentUser = this.listUserFromLS.find(x => x.id === this.userId);
-  }
+  private indexItemActiveInEdit: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: ProjectService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
-    this.service.clickGetUser.subscribe(dataService => {
-      this.currentUser = dataService;
-      this.messages = this.currentUser.messages;
+    this.userId = this.route.snapshot.paramMap.get('id');
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.messages = this.currentUser.messages;
+    console.log(this.messages);
+    this.service.getCurrentUser.subscribe((data) => {
+      this.currentUser = data;
+      this.messages = data.messages;
       this.sendMessge = '';
       this.isEdit = false;
       this.pinMessageBox = false;
-    })
+    });
+    this.users = JSON.parse(localStorage.getItem('users')) || [];
+    this.pinMessageBox = false;
     let today = new Date();
-    this.timeSystem = today.getHours() + ":" + today.getMinutes();
-    this.messages = this.currentUser.messages;
+    this.timeSystem = today.getHours() + ':' + today.getMinutes();
   }
 
   public sendMessage = () => {
@@ -52,60 +55,68 @@ export class ContentComponent implements OnInit {
       message: this.sendMessge,
       id: uuid.v4(),
       clientMessage: true,
-      timeMessage: this.timeSystem
-    }
+      timeMessage: this.timeSystem,
+    };
     this.addMessage_LS(objMessage);
     this.sendMessageSystem();
-  }
+  };
 
   private addMessage_LS = (objMessage) => {
-    ++this.currentUser.totalCountMessages;
-    this.currentUser.lastSendMessage = objMessage.message;
-    this.currentUser.idLastMessage = objMessage.id
-    this.currentUser.messages.push(objMessage);
-    window.localStorage.setItem('user', JSON.stringify(this.listUserFromLS));
+    const { name, family, id, totalCountMessages , userSystem } = this.currentUser;
+    if (totalCountMessages === 0 && userSystem === false) {
+      const user: users = {
+        name,
+        id,
+        family,
+        phone: '',
+        totalCountMessages: 1,
+        idLastMessage: objMessage.id,
+        lastSendMessage: objMessage.message,
+        userSystem : true,
+        messages: [objMessage],
+      };
+      this.currentUser.messages.push(objMessage);
+      this.currentUser = user;
+      localStorage.setItem('currentUser', JSON.stringify(this.currentUser));
+      this.users.push(user);
+      localStorage.setItem('users', JSON.stringify(this.users));
+      this.service.updateUsers(user);
+    } else {
+      const user = this.users.find((x) => x.id === id);
+      ++user.totalCountMessages;
+      ++this.currentUser.totalCountMessages;
+      user.lastSendMessage = objMessage.message;
+      user.idLastMessage = objMessage.id;
+      this.currentUser.lastSendMessage = objMessage.message;
+      this.currentUser.idLastMessage = objMessage.id;
+      // this.currentUser.messages.push(objMessage);
+      this.messages.push(objMessage);
+      user.messages.push(objMessage);
+      localStorage.setItem('users', JSON.stringify(this.users));
+      localStorage.setItem('currentUser', JSON.stringify(this.currentUser));
+      this.service.updateUsers(user);
+    }
     this.sendMessge = '';
-    this.service.updateUsers(true);
-  }
+  };
 
   private sendMessageSystem = () => {
     const objMessage = {
       message: 'testMessageSystem',
       id: uuid.v4(),
       clientMessage: false,
-      timeMessage: this.timeSystem
-    }
-    this.addMessage_LS(objMessage)
-  }
-
-  public isEditMessage = () => {
-    this.messages[this.indexItemActiveInEdit].message = this.sendMessge;
-    // if edit lastSendMessage
-    if (this.messages[this.indexItemActiveInEdit].id === this.currentUser.idLastMessage) {
-      console.log('true lastMessages')
-      this.currentUser.lastSendMessage = this.sendMessge;
-    }
-    if (this.messages[this.indexItemActiveInEdit].pin === true) {
-      this.messageBoxPin = this.sendMessge;
-    }
-    this.listUserFromLS.find(x => x.id === this.userId).messages = this.messages;
-    window.localStorage.setItem('user', JSON.stringify(this.listUserFromLS))
-    this.isEdit = false;
-    this.viewEditMessage = false;
-    this.sendMessge = '';
-    this.showIconOperationById = '';
-    this.service.updateUsers(true);
-  }
+      timeMessage: this.timeSystem,
+    };
+    // this.addMessage_LS(objMessage);
+  };
 
   public openListOperation = (item, index): any => {
     if (this.showIconOperationById === item.id) {
-      return this.showIconOperationById = '';
+      return (this.showIconOperationById = '');
     }
     this.showIconOperationById = item.id;
+  };
 
-  }
-
-  public reply = (item, index) => { }
+  public reply = (item, index) => {};
 
   public edit = (item, index) => {
     this.sendMessge = item.message;
@@ -113,51 +124,109 @@ export class ContentComponent implements OnInit {
     this.isEdit = true;
     this.viewEditMessage = true;
     this.indexItemActiveInEdit = index;
-    this.messages[index].showIconOperation = false;
-  }
+    this.showIconOperationById = '';
+  };
+
+  public isEditMessage = () => {
+    this.messages[this.indexItemActiveInEdit].message = this.sendMessge;
+    // if edit lastSendMessage
+    if ( this.messages[this.indexItemActiveInEdit].id === this.currentUser.idLastMessage ) {
+      this.currentUser.lastSendMessage = this.sendMessge;
+      this.currentUser.messages[this.indexItemActiveInEdit].message = this.sendMessge;
+      const indexUser = this.users.findIndex(
+        (x) => x.id === this.currentUser.id
+      );
+      this.users[indexUser].lastSendMessage = this.sendMessge;
+      this.users[indexUser].messages[this.indexItemActiveInEdit].message =
+        this.sendMessge;
+      localStorage.setItem('currentUser', JSON.stringify(this.currentUser));
+      localStorage.setItem('users', JSON.stringify(this.users));
+      this.service.updateUsers(this.currentUser);
+    } else {
+      this.currentUser.messages[this.indexItemActiveInEdit].message =
+        this.sendMessge;
+      const indexUser = this.users.findIndex(
+        (x) => x.id === this.currentUser.id
+      );
+      this.users[indexUser].messages[this.indexItemActiveInEdit].message =
+        this.sendMessge;
+      localStorage.setItem('currentUser', JSON.stringify(this.currentUser));
+      localStorage.setItem('users', JSON.stringify(this.users));
+    }
+    if (this.messages[this.indexItemActiveInEdit].pin === true) {
+      this.messageBoxPin = this.sendMessge;
+    }
+    this.isEdit = false;
+    this.viewEditMessage = false;
+    this.sendMessge = '';
+    this.showIconOperationById = '';
+  };
 
   public pin = (item, index) => {
     this.messageBoxPin = item.message;
     this.pinMessageBox = true;
-    this.messages[index].showIconOperation = false;
-    this.messages.map(x => {
+    // this.messages[index].showIconOperation = false;
+    this.messages.map((x) => {
       x.pin = false;
-    })
+    });
     this.messages[index].pin = true;
-  }
+  };
 
-  public forward = (item, index) => {
-    item.forwarded = this.currentUser.name;
+  public forward = (item : messages, index : number) => {
+    // item.forwarded = this.currentUser.name;
     item.clientMessage = true;
+    const data = {
+      users : this.users,
+      currentUser : this.currentUser,
+      forwardMessage : item
+    }
     const dialogRef = this.dialog.open(ForwardMessageComponent, {
       width: '35%',
       panelClass: 'modal-create-chanel',
-      data: item
-    })
-  }
+      data,
+    });
+  };
 
   public deleted = (item, index) => {
-    this.messages = this.messages.filter(x => x.id !== item.id);
-    if (item.id === this.currentUser.idLastMessage && index !== 0) {
-      this.currentUser.lastSendMessage = this.messages[index - 1].message;
+    const messageIndex = this.currentUser.messages.findIndex(
+      (x) => x.id === item.id
+    );
+    const userIndex = this.users.findIndex((x) => x.id === this.currentUser.id);
+
+    if (
+      this.currentUser.idLastMessage === item.id &&
+      this.messages.length > 1
+    ) {
       this.currentUser.idLastMessage = this.messages[index - 1].id;
-    } else {
-      this.currentUser.lastSendMessage = '';
+      this.currentUser.lastSendMessage = this.messages[index - 1].message;
+      this.users[userIndex].idLastMessage = this.messages[index - 1].id;
+      this.users[userIndex].lastSendMessage = this.messages[index - 1].message;
+    } 
+    if (this.messages.length === 1) {
+      this.currentUser.totalCountMessages = 0;
+      this.users[userIndex].totalCountMessages = 0;
       this.currentUser.idLastMessage = '';
+      this.currentUser.lastSendMessage = '';
+      this.users[userIndex].idLastMessage = '';
+      this.users[userIndex].lastSendMessage = '';
     }
-    this.listUserFromLS.find(x => x.id === this.userId).messages = this.messages;
-    window.localStorage.setItem('user', JSON.stringify(this.listUserFromLS))
-    this.service.updateUsers(true);
-  }
+    this.currentUser.totalCountMessages -= 1;
+    this.currentUser.messages.splice(messageIndex, 1);
+    this.users[userIndex].totalCountMessages -= 1;
+    this.users[userIndex].messages.splice(messageIndex, 1);
+    this.messages = this.messages.filter((x) => x.id !== item.id);   
+    localStorage.setItem('currentUser', JSON.stringify(this.currentUser));
+    localStorage.setItem('users', JSON.stringify(this.users));
+    this.service.updateUsers(this.currentUser);
+  };
 
   public unPin = () => {
     this.pinMessageBox = false;
-  }
+  };
 
   public unViewEditMessage = () => {
     this.viewEditMessage = false;
     this.isEdit = false;
     this.sendMessge = '';
-  }
-
+  };
 }

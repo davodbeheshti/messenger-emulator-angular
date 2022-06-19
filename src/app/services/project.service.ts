64566f96ac67296a8b17ed08c720e9529736933f
davodbeheshti <davood.beheshti2018@gmail.copm>
2022-06-19 11:20:56 -0700

    changes service project

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,3 +1,4 @@
+import { users, CurentUser } from '../shared/IModelProject';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
@@ -8,21 +9,36 @@ export class ProjectService {
 
   constructor() { }
 
-  clickGetUser = new Subject();
+  clickGetUser = new Subject<users>();
   clickCloseMenu = new Subject();
-  clickUpdateUser = new Subject<boolean>()
+  clickUpdateUser = new Subject<any>()
+  getCurrentUser = new Subject<CurentUser>()
+
 
   getUser(data) {
     this.clickGetUser.next(data);
-    console.log(data)
   }
 
   closeMenu() {
     this.clickCloseMenu.next();
   }
 
-  updateUsers (data : boolean) {
+
+  updateUsers (data) {
     this.clickUpdateUser.next(data)
+  }
+
+  updateContacts(data : CurentUser) {
+    // const getCurrentUser = JSON.parse(localStorage.getItem('users'));
+    // const user = getCurrentUser.find(user => user.id === data.id);
+    localStorage.setItem('currentUser' , JSON.stringify(data))
+    this.getCurrentUser.next(data);
+  }
+  
+  updateCurrentUser_fromContacts(data) {
+    const getCurrentUser = JSON.parse(localStorage.getItem('users'));
+    const user = getCurrentUser.find(user => user.id === data.id);
+    this.getCurrentUser.next(user);
   }
  
 }

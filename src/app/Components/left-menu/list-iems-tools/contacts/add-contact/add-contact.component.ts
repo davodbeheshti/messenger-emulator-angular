dd35d74d73d4c132a1cb05d50a55f03333e736ab
davodbeheshti <davood.beheshti2018@gmail.copm>
2022-06-19 11:16:52 -0700

    fixes bug addContactg

--- src/app/Components/left-menu/list-iems-tools/contacts/add-contact/add-contact.component.ts
+++ src/app/Components/left-menu/list-iems-tools/contacts/add-contact/add-contact.component.ts
@@ -1,3 +1,8 @@
+import {
+  Contact,
+  CurentUser,
+  users,
+} from '../../../../../shared/IModelProject';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
@@ -5,54 +10,58 @@ import { Router } from '@angular/router';
 import { ProjectService } from 'src/app/services/project.service';
 import * as uuid from 'uuid';
 
-
 @Component({
   selector: 'app-add-contact',
   templateUrl: './add-contact.component.html',
   styleUrls: ['./add-contact.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class AddContactComponent implements OnInit {
-
-  constructor(private dialogRef: MatDialogRef<AddContactComponent> , private router : Router , private service : ProjectService) { }
+  constructor(
+    private dialogRef: MatDialogRef<AddContactComponent>,
+    private router: Router,
+    private service: ProjectService
+  ) {}
 
   form: FormGroup;
 
   ngOnInit(): void {
     this.form = new FormGroup({
       name: new FormControl('', Validators.required),
-      lastName: new FormControl(''),
+      family: new FormControl(''),
       phone: new FormControl(''),
-    })
+    });
   }
 
   public cancel = () => {
     this.dialogRef.close();
-  }
-  public create = () => {
-    const dataLocalStorage = window.localStorage.getItem('user');
-    if (this.form.valid) {
-      const { name, lastName, phone } = this.form.value;
-      const data = {
-        name,
-        lastName,
-        phone,
-        id : uuid.v4(),
-        totalCountMessages : 0,
-        messages : [],
-        lastSendMessage : '',
-      };
-      // debugger
-        let arrayLocalStorage = [];
-        arrayLocalStorage = JSON.parse(dataLocalStorage) || [];
-        arrayLocalStorage.push(data);
-        window.localStorage.setItem('user' , JSON.stringify(arrayLocalStorage));
-        this.router.navigateByUrl(`contact/${data.id}`)
-        this.service.getUser(data);
-        this.service.closeMenu();
-        this.dialogRef.close();
-        this.service.updateUsers(true);
-    }
-  }
+  };
 
+  public create = () => {
+    const getContacts = localStorage.getItem('contacts');
+    if (this.form.valid) {
+      const { name, family, phone } = this.form.value;
+      let id = uuid.v4();
+      const contact: CurentUser = {
+        name,
+        family,
+        id,
+        phone,
+        totalCountMessages: 0,
+        messages: [],
+        lastSendMessage: '',
+        idLastMessage: '',
+        userSystem : false,
+      };
+      let listContacts = JSON.parse(getContacts) || [];
+      listContacts.push(contact);
+      localStorage.setItem('contacts', JSON.stringify(listContacts));
+      localStorage.setItem('currentUser', JSON.stringify(contact));
+      this.service.updateContacts(contact);
+      this.router.navigateByUrl(`contact/${contact.id}`);
+      this.service.closeMenu();
+      this.dialogRef.close();
+      // this.service.updateUsers(true);
+    }
+  };
 }

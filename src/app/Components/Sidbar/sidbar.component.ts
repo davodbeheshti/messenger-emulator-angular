550a2ceacca0d52f5900b16f2c12527e5a49885e
davodbeheshti <davood.beheshti2018@gmail.copm>
2022-06-19 11:20:16 -0700

    fixes changes sidbar

--- src/app/Components/Sidbar/sidbar.component.ts
+++ src/app/Components/Sidbar/sidbar.component.ts
@@ -1,66 +1,59 @@
+import { messages } from '../../shared/IModelProject';
 import { Component, OnInit, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 import { users } from 'src/app/shared/IModelProject';
 
-import { ProjectService } from '../../services/project.service'
+import { ProjectService } from '../../services/project.service';
 
 @Component({
   selector: 'app-sidbar',
   templateUrl: './sidbar.component.html',
-  styleUrls: ['./sidbar.component.scss']
+  styleUrls: ['./sidbar.component.scss'],
 })
 export class SidbarComponent implements OnInit {
-
   @Output() clickHanbergerMenu = new EventEmitter();
-  users;
   users: users[];
   showMenuOperationById: string = '';
-  listUserFromLS;
-  currentUser;
-  constructor(private router: Router, private service: ProjectService) {
-    this.listUserFromLS = JSON.parse(window.localStorage.getItem('user'));
-    // this.currentUser = ;
-  }
+  currentUser: users;
+  constructor(private router: Router, private service: ProjectService) {}
 
   ngOnInit(): void {
-    this.users = JSON.parse(window.localStorage.getItem('user'));
-
-    this.service.clickUpdateUser.subscribe(x => {
-      if(x) {
-        this.users = JSON.parse(window.localStorage.getItem('user'));
-        console.log(this.users)
-      }
-    })
+    this.users = JSON.parse(localStorage.getItem('users'));
+    
+    this.service.clickUpdateUser.subscribe((data) => {
+      this.users = JSON.parse(localStorage.getItem('users'));
+      const indexUser = this.users.findIndex(user => user.id === data.id);
+      this.users[indexUser] = data;
+    });
   }
 
   public openLeftMenu = () => {
-    console.log("clickSlider")
     this.clickHanbergerMenu.emit();
-  }
+  };
 
   public clickBoxUser = (item, i) => {
     this.router.navigateByUrl(`contact/${item.id}`);
-    this.service.getUser(item)
-    console.log(item)
-  }
+    this.service.updateContacts(item);
+    // this.service.getUser(item)
+  };
 
   public clickShowMenuOperation = (item, i): any => {
     if (this.showMenuOperationById === item.id) {
-      return this.showMenuOperationById = '';
+      return (this.showMenuOperationById = '');
     }
-    this.showMenuOperationById  = this.users[i].id;
-  }
-  public clearHistory = (item, i) => {
-    const currentUser = this.listUserFromLS.find(x => x.id === item.id)
-    currentUser.messages = [];
-    window.localStorage.setItem('user', JSON.stringify(this.listUserFromLS));
-  }
-  public deleteChate = (item, i) => {
-    this.users = this.users.filter(x => x.id !== item.id);
-    this.listUserFromLS = this.listUserFromLS.filter(x => x.id !== item.id);
-    window.localStorage.setItem('user', JSON.stringify(this.listUserFromLS));
-    this.router.navigate(['/']);
-  }
+    this.showMenuOperationById = this.users[i].id;
+  };
 
+  public clearHistory = (item, i) => {
+    const currentUser = this.users.find((x) => x.id === item.id);
+    currentUser.messages = [];
+    localStorage.setItem('users', JSON.stringify(this.users));
+  };
+
+  public deleteChate = (item, i) => {
+    this.users = this.users.filter((x) => x.id !== item.id);
+    localStorage.setItem('users', JSON.stringify(this.users));
+    this.router.navigate(['/']);
+  };
 }

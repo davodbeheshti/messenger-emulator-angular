3a51c42aa9606aab05de097519a6717765ecbd79
davodbeheshti <davood.beheshti2018@gmail.com>
2023-03-17 13:13:32 -0700

    just activity in github

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,7 +10,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { FilterSearchListPipe } from './filter-search-list.pipe';
 
-
+ 
  
 const importComponentMaterial = [
   MatDialogModule,

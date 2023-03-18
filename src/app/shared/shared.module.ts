2d43fc67933b473ed0d3808058f29274e9376c9d
davodbeheshti <davood.beheshti2018@gmail.com>
2023-03-18 13:02:53 -0700

    justi activity github

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -11,7 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { FilterSearchListPipe } from './filter-search-list.pipe';
 import { DialogConfirmComponent } from './dialog-confirm/dialog-confirm.component';
 
- 
+  
  
 const importComponentMaterial = [
   MatDialogModule,

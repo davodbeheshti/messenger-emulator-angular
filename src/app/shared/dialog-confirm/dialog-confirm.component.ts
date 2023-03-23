5c3ec67a656274dc2262be1aa749ef566b1bf502
davodbeheshti <davood.beheshti2018@gmail.com>
2023-03-23 08:28:24 -0700

    adding customeComponent dialog to delete user and message

---
+++ src/app/shared/dialog-confirm/dialog-confirm.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-confirm',
+  templateUrl: './dialog-confirm.component.html',
+  styleUrls: ['./dialog-confirm.component.scss']
+})
+export class DialogConfirmComponent implements OnInit {
+
+ 
+}
